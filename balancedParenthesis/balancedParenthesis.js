b9022ca2f3af5293fb25f58bad14f3b0358c2643
Klerta <tklerta@gmail.com>
2024-09-30 17:57:00 -0400

    ilesx`

--- balancedParenthesis/balancedParenthesis.js
+++ balancedParenthesis/balancedParenthesis.js
@@ -13,3 +13,33 @@ const isBalanced = function (str) {
   
     return stack.length === 0;
   };
+const isBalanceed = function (str) {
+    const stack = [];
+  
+    for (let char of str) {
+      if (char === '(') {
+        stack.push(char);
+      } else if (char === ')') {
+        if (stack.pop() !== '(') {
+          return false;
+        }
+      }
+    }
+  
+    return stack.length === 0;
+  };
+const isBalancedd = function (str) {
+    const stack = [];
+  
+    for (let char of str) {
+      if (char === '(') {
+        stack.push(char);
+      } else if (char === ')') {
+        if (stack.pop() !== '(') {
+          return false;
+        }
+      }
+    }
+  
+    return stack.length === 0;
+  };
