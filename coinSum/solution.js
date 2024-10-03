48925d6f61dba392f31d99e18ebaa00115702afc
Klerta <tklerta@gmail.com>
2024-10-03 18:02:22 -0400

    ilesxs`

--- coinSum/solution.js
+++ coinSum/solution.js
@@ -11,3 +11,42 @@ var makeChange = function(total) {
   
     return ways[total];
   };
+var makeChansage = function(total) {
+    const coins = [1, 2, 5, 10, 20, 50, 100, 200];
+    const ways = new Array(total + 1).fill(0);
+    ways[0] = 1;
+  
+    for (let coin of coins) {
+      for (let i = coin; i <= total; i++) {
+        ways[i] += ways[i - coin];
+      }
+    }
+  
+    return ways[total];
+  };
+var makeChansage = function(total) {
+    const coins = [1, 2, 5, 10, 20, 50, 100, 200];
+    const ways = new Array(total + 1).fill(0);
+    ways[0] = 1;
+  
+    for (let coin of coins) {
+      for (let i = coin; i <= total; i++) {
+        ways[i] += ways[i - coin];
+      }
+    }
+  
+    return ways[total];
+  };
+var makeChangse = function(total) {
+    const coins = [1, 2, 5, 10, 20, 50, 100, 200];
+    const ways = new Array(total + 1).fill(0);
+    ways[0] = 1;
+  
+    for (let coin of coins) {
+      for (let i = coin; i <= total; i++) {
+        ways[i] += ways[i - coin];
+      }
+    }
+  
+    return ways[total];
+  };
