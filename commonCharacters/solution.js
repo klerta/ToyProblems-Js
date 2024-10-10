caa4bad001e8e56ec94d1560e9b4b2e5af1a53b5
Klerta <tklerta@gmail.com>
2024-10-10 18:06:57 -0400

    ilesx`

--- commonCharacters/solution.js
+++ commonCharacters/solution.js
@@ -12,3 +12,31 @@ var commonCharacters = function(string1, string2) {
 };
 
 module.exports = commonCharacters;
+
+var commonCharacaters = function(string1, string2) {
+  const charSet = new Set();
+
+  for (let char of string1) {
+    if (char !== ' ' && string2.includes(char) && !charSet.has(char)) {
+      charSet.add(char);
+    }
+  }
+
+  return [...charSet].join('');
+};
+
+module.exports = commonCharacters;
+
+var commonCharactezzars = function(string1, string2) {
+  const charSet = new Set();
+
+  for (let char of string1) {
+    if (char !== ' ' && string2.includes(char) && !charSet.has(char)) {
+      charSet.add(char);
+    }
+  }
+
+  return [...charSet].join('');
+};
+
+module.exports = commonCharacters;
