8570aaeac46b58d85a73988b34969fe4739127b5
Klerta <tklerta@gmail.com>
2024-10-17 18:10:45 -0400

    ilesx`

--- deepEquality/solution.js
+++ deepEquality/solution.js
@@ -18,3 +18,63 @@ var deepEquals = function(apple, orange) {
 
   return true;
 };
+var deepEquals = function(apple, orange) {
+  if (typeof apple !== 'object' || typeof orange !== 'object') {
+    return apple === orange;
+  }
+
+  const keysApple = Object.keys(apple);
+  const keysOrange = Object.keys(orange);
+
+  if (keysApple.length !== keysOrange.length) {
+    return false;
+  }
+
+  for (let key of keysApple) {
+    if (!deepEquals(apple[key], orange[key])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+var deepEquals = function(apple, orange) {
+  if (typeof apple !== 'object' || typeof orange !== 'object') {
+    return apple === orange;
+  }
+
+  const keysApple = Object.keys(apple);
+  const keysOrange = Object.keys(orange);
+
+  if (keysApple.length !== keysOrange.length) {
+    return false;
+  }
+
+  for (let key of keysApple) {
+    if (!deepEquals(apple[key], orange[key])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+var deepEquuals = function(apple, orange) {
+  if (typeof apple !== 'object' || typeof orange !== 'object') {
+    return apple === orange;
+  }
+
+  const keysApple = Object.keys(apple);
+  const keysOrange = Object.keys(orange);
+
+  if (keysApple.length !== keysOrange.length) {
+    return false;
+  }
+
+  for (let key of keysApple) {
+    if (!deepEquals(apple[key], orange[key])) {
+      return false;
+    }
+  }
+
+  return true;
+};
