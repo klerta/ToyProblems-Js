b05d004c2f226235b9485089b386e7c4a2d6bd23
Klerta <tklerta@gmail.com>
2024-10-23 18:13:49 -0400

    ilesx`

--- evenOccurence/solution.js
+++ evenOccurence/solution.js
@@ -93,3 +93,288 @@ var evenOccurrence = function(arr) {
 
   return null;
 };
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
+var evenOccurrence = function(arr) {
+  const countMap = new Map();
+
+  for (let item of arr) {
+    if (countMap.has(item)) {
+      countMap.set(item, countMap.get(item) + 1);
+    } else {
+      countMap.set(item, 1);
+    }
+  }
+
+  for (let item of arr) {
+    if (countMap.get(item) % 2 === 0) {
+      return item;
+    }
+  }
+
+  return null;
+};
