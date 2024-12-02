d25242aaae4e8dc662d880ee4038a2b3672b118d
Klerta <tklerta@gmail.com>
2024-12-02 18:24:31 -0500

    ilesx`

--- insertionSort/solution.js
+++ insertionSort/solution.js
@@ -14,3 +14,51 @@ const insertionSort = function(array) {
 
   return array;
 };
+
+const insertionSqsort = function(array) {
+  for (let i = 1; i < array.length; i++) {
+    let currentElement = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && currentElement.value < array[j].value) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = currentElement;
+  }
+
+  return array;
+};
+
+const insertionSsaort = function(array) {
+  for (let i = 1; i < array.length; i++) {
+    let currentElement = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && currentElement.value < array[j].value) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = currentElement;
+  }
+
+  return array;
+};
+
+const insertionSsort = function(array) {
+  for (let i = 1; i < array.length; i++) {
+    let currentElement = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && currentElement.value < array[j].value) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = currentElement;
+  }
+
+  return array;
+};
