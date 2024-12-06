0a4514a8d924ebced812327c424b7013749ade6b
Klerta <tklerta@gmail.com>
2024-12-06 18:26:20 -0500

    ilesx`

--- isSubsetOf/solution.js
+++ isSubsetOf/solution.js
@@ -22,3 +22,75 @@ Array.prototype.isSubsetOf = function(arr) {
   const inputSet = new Set(arr);
   return this.every(element => inputSet.has(element));
 };
+/**
+ * Determine whether the context array is a subset of the input array.
+ *
+ * @function isSubsetOf
+ * @param {Array} arr - The input array.
+ * @return {Boolean} - Returns true if the context array is a subset of the input array, false otherwise.
+ */
+Array.prototype.isSubsetOf = function(arr) {
+  // Use Set data structure to efficiently check for subset
+  const inputSet = new Set(arr);
+  return this.every(element => inputSet.has(element));
+};
+/**
+ * Determine whether the context array is a subset of the input array.
+ *
+ * @function isSubsetOf
+ * @param {Array} arr - The input array.
+ * @return {Boolean} - Returns true if the context array is a subset of the input array, false otherwise.
+ */
+Array.prototype.isSubsetOf = function(arr) {
+  // Use Set data structure to efficiently check for subset
+  const inputSet = new Set(arr);
+  return this.every(element => inputSet.has(element));
+};
+/**
+ * Determine whether the context array is a subset of the input array.
+ *
+ * @function isSubsetOf
+ * @param {Array} arr - The input array.
+ * @return {Boolean} - Returns true if the context array is a subset of the input array, false otherwise.
+ */
+Array.prototype.isSubsetOf = function(arr) {
+  // Use Set data structure to efficiently check for subset
+  const inputSet = new Set(arr);
+  return this.every(element => inputSet.has(element));
+};
+/**
+ * Determine whether the context array is a subset of the input array.
+ *
+ * @function isSubsetOf
+ * @param {Array} arr - The input array.
+ * @return {Boolean} - Returns true if the context array is a subset of the input array, false otherwise.
+ */
+Array.prototype.isSubsetOf = function(arr) {
+  // Use Set data structure to efficiently check for subset
+  const inputSet = new Set(arr);
+  return this.every(element => inputSet.has(element));
+};
+/**
+ * Determine whether the context array is a subset of the input array.
+ *
+ * @function isSubsetOf
+ * @param {Array} arr - The input array.
+ * @return {Boolean} - Returns true if the context array is a subset of the input array, false otherwise.
+ */
+Array.prototype.isSubsetOf = function(arr) {
+  // Use Set data structure to efficiently check for subset
+  const inputSet = new Set(arr);
+  return this.every(element => inputSet.has(element));
+};
+/**
+ * Determine whether the context array is a subset of the input array.
+ *
+ * @function isSubsetOf
+ * @param {Array} arr - The input array.
+ * @return {Boolean} - Returns true if the context array is a subset of the input array, false otherwise.
+ */
+Array.prototype.isSubsetOf = function(arr) {
+  // Use Set data structure to efficiently check for subset
+  const inputSet = new Set(arr);
+  return this.every(element => inputSet.has(element));
+};
