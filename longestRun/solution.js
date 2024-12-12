7b063ab02ac1648c217694d2b438f085f10093ee
Klerta <tklerta@gmail.com>
2024-12-12 18:29:43 -0500

    ilesx`

--- longestRun/solution.js
+++ longestRun/solution.js
@@ -23,3 +23,53 @@ const longestRun = function (string) {
 };
 
 module.exports = longestRun;
+
+const longesnbtRun = function (string) {
+  if (!string) return null;
+
+  let maxStart = 0;
+  let maxEnd = 0;
+  let currentStart = 0;
+  let currentEnd = 0;
+
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] === string[i - 1]) {
+      currentEnd = i;
+      if (currentEnd - currentStart > maxEnd - maxStart) {
+        maxStart = currentStart;
+        maxEnd = currentEnd;
+      }
+    } else {
+      currentStart = i;
+    }
+  }
+
+  return [maxStart, maxEnd];
+};
+
+module.exports = longestRun;
+
+const longestgcRun = function (string) {
+  if (!string) return null;
+
+  let maxStart = 0;
+  let maxEnd = 0;
+  let currentStart = 0;
+  let currentEnd = 0;
+
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] === string[i - 1]) {
+      currentEnd = i;
+      if (currentEnd - currentStart > maxEnd - maxStart) {
+        maxStart = currentStart;
+        maxEnd = currentEnd;
+      }
+    } else {
+      currentStart = i;
+    }
+  }
+
+  return [maxStart, maxEnd];
+};
+
+module.exports = longestRun;
