48a87a23e804f75cc90e0d3a22849050e5b34652
Klerta <tklerta@gmail.com>
2024-12-19 18:32:10 -0500

    ilesx`

--- mergeSort/solution.js
+++ mergeSort/solution.js
@@ -29,3 +29,34 @@ const mergeSort = function(array) {
 };
 
 module.exports = mergeSort;
+const mergeeSort = function(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  const merge = (left, right) => {
+    const result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        result.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+  };
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+module.exports = mergeSort;
